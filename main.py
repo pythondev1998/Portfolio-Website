1ec7894e64b6389ac8f05d99eda9fc1bd138f3e0
pythondev1998 <pythondevelopcode@gmail.com>
2023-07-24 00:45:40 -0400

    port fix

--- main.py
+++ main.py
@@ -27,16 +27,11 @@ def send_email():
         subject = f'Contacto de {fullname}'
         msg_body = f'Nombre: {fullname}\nCorreo electrónico: {email}\nMensaje: {message}'
 
-        # Send the email
         yag.send(to=[GMAIL_USERNAME], subject=subject, contents=msg_body)
 
-        # Return success message or redirect to a success page
-        return render_template('contact_success.html')
-
     except Exception as e:
-        # Return error message or redirect to an error page
+       
         return render_template('contact_error.html')
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
 
